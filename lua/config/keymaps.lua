af332d4751ef0d137e74a9915fbf8bf0afa617e6
Daniel Haarhoff <daniel@rknt.de>
2026-02-17 19:39:57 +0000

    Make CodeCompanion usable

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,8 +2,13 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
--- Free up l for sllm
+-- Free up l for LLM stuff
 vim.keymap.del("n", "<leader>L")
 vim.keymap.del("n", "<leader>l")
 vim.keymap.set("n", "<leader>L", "<cmd>Lazy<cr>", { desc = "Lazy" })
 vim.keymap.set("n", "<leader>r", "<cmd>checktime<cr>", { desc = "Check for external buffer changes" })
+
+-- CodeCompanion
+vim.keymap.set("n", "<leader>l", "<cmd>CodeCompanionChat Toggle<cr>", { desc = "Toggle chat buffer" })
+vim.keymap.set("v", "<leader>l", ":'<, '>CodeCompanion<cr> ", { desc = "Inline assistant" })
+vim.keymap.set("n", "<leader>ln", "<cmd>CodeCompanionChat<cr>", { desc = "New chat buffer" })
--- lua/plugins/codecompanion.lua
+++ lua/plugins/codecompanion.lua
@@ -1,7 +1,34 @@
 return {
   "olimorris/codecompanion.nvim",
   version = "^18.0.0",
-  opts = {},
+  opts = {
+    adapters = {
+      http = {
+        cerebras = function()
+          return require("codecompanion.adapters").extend("openai_compatible", {
+            env = {
+              url = "https://api.cerebras.ai",
+              api_key = "CEREBRAS_API_KEY",
+              chat_url = "/v1/chat/completions",
+            },
+            schema = {
+              model = {
+                default = "gpt-oss-120b",
+              },
+            },
+          })
+        end,
+      },
+    },
+    interactions = {
+      chat = {
+        adapter = "cerebras",
+      },
+      inline = {
+        adapter = "cerebras",
+      },
+    },
+  },
   dependencies = {
     "nvim-lua/plenary.nvim",
     "nvim-treesitter/nvim-treesitter",
