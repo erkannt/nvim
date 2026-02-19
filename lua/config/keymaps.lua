3961e16c70d2a004acc33bdc32d45bd15087d7bd
Daniel Haarhoff <daniel@rknt.de>
2026-02-19 16:57:03 +0000

    keymap to toggle virtual text diagnostics

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -12,3 +12,16 @@ vim.keymap.set("n", "<leader>r", "<cmd>checktime<cr>", { desc = "Check for exter
 vim.keymap.set("n", "<leader>l", "<cmd>CodeCompanionChat Toggle<cr>", { desc = "Toggle chat buffer" })
 vim.keymap.set("v", "<leader>l", ":'<, '>CodeCompanion<cr> ", { desc = "Inline assistant" })
 vim.keymap.set("n", "<leader>ln", "<cmd>CodeCompanionChat<cr>", { desc = "New chat buffer" })
+
+-- Diagnostics toggle
+local function toggle_virtual_text()
+  local config = vim.diagnostic.config()
+  local current = config.virtual_text
+  vim.diagnostic.config({ virtual_text = not current })
+  vim.notify(
+    (not current and "Enabled" or "Disabled") .. " virtual text diagnostics",
+    vim.log.levels.INFO,
+    { title = "Diagnostics" }
+  )
+end
+vim.keymap.set("n", "<leader>ue", toggle_virtual_text, { desc = "Toggle virtual text diagnostics" })
