a87ffada9ca78c231c73028a6231c1f7703adceb
Daniel Haarhoff <daniel@rknt.de>
2026-06-18 16:07:06 +0100

    Remove llm stuff as I use other tools for llm interaction

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,20 +2,6 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
--- Free up l for LLM stuff
-vim.keymap.del("n", "<leader>L")
-vim.keymap.del("n", "<leader>l")
-vim.keymap.set("n", "<leader>L", "<cmd>Lazy<cr>", { desc = "Lazy" })
-
--- CodeCompanion
-vim.keymap.set("n", "<leader>l", "<cmd>CodeCompanionChat Toggle<cr>", { desc = "Toggle chat buffer" })
-vim.keymap.set("v", "<leader>l", ":'<, '>CodeCompanion<cr> ", { desc = "Inline assistant" })
-vim.keymap.set("n", "<leader>ln", "<cmd>CodeCompanionChat<cr>", { desc = "New chat buffer" })
-
--- gen.nvim (LLM generation)
-vim.keymap.set({ "n", "v" }, "<leader>g", ":Gen<CR>", { desc = "Generate with LLM" })
-vim.keymap.set("n", "<leader>gg", ":Gen Chat<CR>", { desc = "Chat with LLM" })
-
 -- Diagnostics toggle
 local function toggle_virtual_text()
   local config = vim.diagnostic.config()
